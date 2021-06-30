579bb9180d80206c4dd50871a81da1c2aadc2419
Jeremy Huddleston <jshudd@gmail.com>
2021-06-30 14:48:29 -0500

    testing push

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const plus = (num) => {
+  return (plusNum) => num + plusNum;
+};
+
+const plus15 = plus(15);
+
+console.log(plus15(10)); //prints 25
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(plus15(...numbers)); //prints 16
+console.log(plus15(1, 2, 3, 4, 5)); //prints 16; only uses 1 because function on takes in 1 parameter
